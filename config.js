59a0013e89b4a1a10d9684e0eb182bd3cb8dde1e
luis_duran <136107433+luisduran890815@users.noreply.github.com>
2026-09-09 13:02:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://tjtejaepwmkomivkpbjx.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_ZeK1mssaCbgoJuKRagTD0g_l2kaqc6G'
+  SUPABASE_URL: '',
+  SUPABASE_ANON_KEY: ''
 };
